0d9f5799591be0d4d9f3c092d158f39abca1826b
dolphius21 <johnrudolphlee@gmail.com>
2021-03-22 12:18:13 +0800

    removed env file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+// CSS
+import './index.css';
 
-function Greeting() {
-  return <h4>Hello World</h4>;
-}
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
+};
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg"
+        alt=""
+      />
+      <h1>I Love You to the Moon and Back</h1>
+      <p style={{ color: '#617d98', fontSize: '0.75rem' }}>Amelia Hepworth, Tim Warnes</p>
+    </article>
+  );
+};
+
+ReactDom.render(<Booklist />, document.getElementById('root'));
